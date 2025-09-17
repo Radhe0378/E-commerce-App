0222942bb5bcc59e93131144d26f91694e7ae5fd
Radhe0378 <radhegarg2003@gmail.com>
2025-09-17 09:23:10 +0530

    Initial commit - project setup

---
+++ frontend/src/main.jsx
@@ -0,0 +1,17 @@
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import './index.css'
+import App from './App.jsx'
+import {BrowserRouter} from 'react-router-dom'
+
+//using BrowserRouter so that we get the support of react-router-dom in our whole project
+
+import ShopContextProvider from './context/ShopContext.jsx'
+
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <BrowserRouter>
+    <ShopContextProvider>
+      <App />
+    </ShopContextProvider>
+  </BrowserRouter>,
+)
